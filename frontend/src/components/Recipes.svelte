4b1afc012f00833795878443e6ba6dc8dee0c361
Wolfgang Becker <wolli.becker@gmail.com>
2019-12-31 15:43:36 -0600

    Fetch recipes from mock server

--- frontend/src/components/Recipes.svelte
+++ frontend/src/components/Recipes.svelte
@@ -1,11 +1,23 @@
 <script>
+  import axios from "axios";
+  import { onMount } from "svelte";
   import Recipe from "./Recipe.svelte";
 
+  let promise = Promise.resolve({data: []});
 
+  onMount(() => {
+    promise = axios.get("http://localhost:3000/recipes").then(response => response.data);
+  });
 </script>
 
-<div class="ui cards">
-  {#each recipes as recipe}
-    <Recipe {...recipe} />
-  {/each}
-</div>
+{#await promise}
+  <div class="ui active massive centered inline loader"></div>
+{:then recipes}
+  <div class="ui cards">
+    {#each recipes as recipe}
+      <Recipe {...recipe} />
+    {/each}
+  </div>
+{:catch error}
+  <p>Failed to fetch recipes.</p>
+{/await}
