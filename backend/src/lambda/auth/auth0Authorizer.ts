fcb521973c38d71bb608cdc91222ae4a97ce61be
Wolfgang Becker <wolli.becker@gmail.com>
2020-01-17 01:24:29 +0100

    Add authentication function for auth0

---
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -0,0 +1,73 @@
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import { verify } from 'jsonwebtoken'
+import Axios from 'axios'
+
+import logger from '../../utils/logger'
+import { JwtPayload } from '../../auth/JwtPayloads'
+
+let certificate;
+const jwksUrl: string = process.env.AUTH0_CERTIFICATE_URL || "";
+
+export const handler = async (
+  event: CustomAuthorizerEvent
+): Promise<CustomAuthorizerResult> => {
+  logger.info('Authorizing a user', event.authorizationToken)
+  try {
+    const jwtToken = await verifyToken(event.authorizationToken as string)
+    logger.info('User was authorized', jwtToken)
+
+    return {
+      principalId: jwtToken.sub,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  } catch (e) {
+    logger.error('User not authorized', { error: e.message })
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Deny',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  }
+}
+
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
+  const token = getToken(authHeader)
+
+  if(!certificate) {
+    const {data} = await Axios.get(jwksUrl)
+    // TODO: Figure out how to read the cert properly
+    certificate = data.keys[0]["x5c"][0]
+  }
+
+  return verify(token, certificate, { algorithms: ['RS256']}) as JwtPayload
+}
+
+function getToken(authHeader: string): string {
+  if (!authHeader) throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+  return token
+}
